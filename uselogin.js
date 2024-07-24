e0d531ad8277a050697d421438b9d00df259c830
rsgltzyd <rsgltzyd@hotmail.com>
2024-07-24 21:58:24 +0800

    fix

--- uselogin.js
+++ uselogin.js
@@ -2,24 +2,15 @@ import { launch } from 'puppeteer';
 import fs from 'fs';
 import 'dotenv/config.js';
 
+import { setLocalStorage } from './utils/userUtils.js';
+
 (async () => {
     const browser = await launch({ headless: false, devtools: true, args: [`--window-size=1920,1080`], defaultViewport: { width: 1920, height: 1080 } });
     const page = await browser.newPage();
     await page.goto('https://dev-dmp.meiguanjia.net/login');
 
-    const file = 'user.json';//获取file
-    const outputLocalStorge = fs.readFileSync(file, 'utf-8');
-    let localStorageTest = JSON.parse(outputLocalStorge);
+    await setLocalStorage(page, 'user.json');
 
-
-    // 设置页面的localStorage
-    await page.evaluateOnNewDocument(localStorageTest => {
-        localStorage.clear();
-        localStorage.setItem('merchant_access_token', localStorageTest.merchant_access_token);
-        localStorage.setItem('merchant_refresh_token', localStorageTest.merchant_refresh_token);
-        localStorage.setItem('merchant_login_data', localStorageTest.merchant_login_data);
-        localStorage.setItem('merchant_metadata_data', localStorageTest.merchant_metadata_data);
-    }, localStorageTest);
     await page.goto('https://dev-dmp.meiguanjia.net/report/businessData');
 
     await browser.close();
---
+++ utils/userUtils.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+
+/**
+ * 设置页面的LocalStorage
+ * @param {*} page 
+ * @param {String} file 用户信息
+ */
+export async function setLocalStorage(page, file) {
+    const outputLocalStorge = fs.readFileSync(file, 'utf-8');
+    let localStorageTest = JSON.parse(outputLocalStorge);
+
+    // 设置页面的localStorage
+    await page.evaluateOnNewDocument(localStorageTest => {
+        localStorage.clear();
+        localStorage.setItem('merchant_access_token', localStorageTest.merchant_access_token);
+        localStorage.setItem('merchant_refresh_token', localStorageTest.merchant_refresh_token);
+        localStorage.setItem('merchant_login_data', localStorageTest.merchant_login_data);
+        localStorage.setItem('merchant_metadata_data', localStorageTest.merchant_metadata_data);
+    }, localStorageTest);
+}

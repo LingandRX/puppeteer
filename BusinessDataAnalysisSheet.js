427cc0d92967668d7003f80121027fbca90ed5f4
wenpeng <wenpeng@meiguanjia.net>
2024-08-05 18:18:38 +0800

    fix

--- BusinessDataAnalysisSheet.js
+++ BusinessDataAnalysisSheet.js
@@ -1,24 +1,31 @@
 import { launch } from 'puppeteer';
+import fs from 'fs';
 import 'dotenv/config.js'
+import { setLocalStorage } from './utils/userUtils.js';
+import { getDate } from './utils/businessDataUtil.js';
+import { setMonthTime } from './utils/commnUtils.js'
 
 (async () => {
     const browser = await launch({ headless: true, args: [`--window-size=1920,1080`], defaultViewport: { width: 1920, height: 1080 } });
     const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 });
+    setLocalStorage(page, '.\\data\\user.json');
     await page.goto('https://dev-dmp.meiguanjia.net/report/businessData');
+    // await page.goto('https://boss.aizhb.net/report/businessData');
 
-    console.log(current);
-    console.log(process.env.ACCOUNT);
-a
-    const item = await page.$eval('.item.cur', node=>node.innerHTML);
-    console.log(item);
-    // const arcoMessage = await page.$eval('.arco-message .arco-message-content', node => node.innerHTML);
-    // await page.screenshot({ path: 'resetPassword.png' });
-    console.log(arcoMessage);
+    await page.waitForSelector('.menu-list-second');
+    const childElements = await page.$$('.menu-list-second .menu-list-second-item');
+    await new Promise(r => setTimeout(r, 1000));
+    await childElements[1].click();
+    const str = await page.evaluate(node => node.textContent.trim(), childElements[1]);
+    console.log(str);
 
-    page.on('console', msg => {
-        console.log('PAGE:', msg.text())
-    })
+    const file = './data/businessData.json';
+
+    fs.writeFileSync(file, '', 'utf-8');
+
+    await setMonthTime(page, '.arco-picker-start-time', '2024-05');
+    await getDate(page, fs, file);
+    console.log('========');
 
     await browser.close();
 })();
